02478240f1ffb1578b7c0a1534b9b993741be5f5
Evgeny <moonlol45@gmail.com>
2026-01-05 10:48:07 +0500

    full menu_skip and chage theme tests

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -10,6 +10,9 @@ export class MainPage extends BasePage {
   private readonly headerAddButtonPopupListLocator: Locator;
   private readonly headerNotificationPopupLocator: Locator;
   private readonly autorizationModalLocator: Locator;
+  private readonly menuButtonLocator: Locator;
+  private readonly openMenuAriaLocator: Locator;
+  private readonly chageThemeButtonLocator: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -31,9 +34,20 @@ export class MainPage extends BasePage {
       .locator('iframe[title="Multipass"]')
       .contentFrame()
       .locator('div[role="form"]');
+    this.menuButtonLocator = this.page.getByRole('button', { name: 'Открыть меню навигации' });
+    this.openMenuAriaLocator = this.page.locator('.menu-content-module__menuOpen');
+    this.chageThemeButtonLocator = this.page.getByRole('button', {
+      name: 'Переключить на светлую тему',
+    });
   }
   async open() {
     await this.page.goto('https://rutube.ru/');
+  }
+  async openFullMenu() {
+    await this.menuButtonLocator.click();
+  }
+  async changeThemeToWhite() {
+    await this.chageThemeButtonLocator.click();
   }
   async headerHasCorrectAriaSnapshot() {
     await expect(this.headerLoactor).toMatchAriaSnapshot({ name: 'headerAriaSnapshot.yml' });
@@ -69,5 +83,11 @@ export class MainPage extends BasePage {
     await expect(this.autorizationModalLocator).toMatchAriaSnapshot({
       name: 'autorizationModal.yml',
     });
+  }
+  async fullMenuHasCorrectAriaShapshot() {
+    await expect(this.openMenuAriaLocator).toMatchAriaSnapshot({ name: 'fullMenuSnapshot.yml' });
+  }
+  async checkThemeAttributeValue(attributeValue: 'dark2021' | 'white2022') {
+    await expect(this.page.locator('html')).toHaveAttribute('data-pen-theme', attributeValue);
   }
 }
